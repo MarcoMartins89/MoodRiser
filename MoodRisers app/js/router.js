9b069f649a0f5ea44297509aa1e70e6302212e56
Marco Martins <marcoibrmartins@gmail.com>
2024-04-17 16:17:24 +0100

    app software structure

---
+++ MoodRisers app/js/router.js
@@ -0,0 +1,55 @@
+import routes from '/js/routes.js'
+
+function setCurrentRoute({ path, controller }) {
+
+	routes.currentPath.path = path;
+	routes.currentPath.controller = controller;
+
+}
+
+async function launchController(controllerName, args) {
+
+	const module = await import(`./controller/${controllerName}.js`);
+	module.default.init(args);
+}
+
+function navigate(path) {
+
+	if (path === routes.currentPath.path) {
+		return;
+	}
+
+	const pathKey = path.split('/')[1];
+
+	console.log("this is path: " + path);
+	console.log("this is path key: " + pathKey);
+	
+	const route = routes[pathKey] || routes.home;
+	console.log('this is controller ' + route.controller);
+
+	setCurrentRoute(route);
+	launchController(route.controller, path.split('/').slice(2));
+
+}
+
+
+function getPath(urlStr) {
+	return new URL(urlStr).hash.slice(1);
+}
+
+function navigateOnHashChange() {
+	addEventListener('hashchange', (e) => {
+		const path = getPath(e.newURL);
+		navigate(path);
+	})
+}
+
+function init() {
+
+	window.location.hash = window.location.hash || routes.home.path;
+
+	navigate(getPath(window.location.href));
+	navigateOnHashChange();
+}
+
+export default { init };
---
+++ MoodRisers app/js/routes.js
@@ -0,0 +1,7 @@
+export default {
+    home: {
+        path: "/",
+        controller: "homeController",
+    },
+    
+};
