6cbad4831e24133b4024e4469c0f7930f78690e5
André Hortelão <andrefhortelao@gmail.com>
2024-04-18 02:06:08 +0100

    MoodTracker

--- MoodRisers app/react-moodrisers/src/App.js
+++ MoodRisers app/react-moodrisers/src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+       <h1>Hi!!</h1>
+       <input type="textbox" name="prompt" placeholder="What is your mood today?" size="100"/>
+       <button>Submit</button>
       </header>
     </div>
   );
